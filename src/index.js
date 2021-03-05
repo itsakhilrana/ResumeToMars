6cc0983dd259bd5af316453f449d10d5b6eb8223
unknown <akhilice13@gmail.com>
2021-03-05 16:48:26 +0530

    Added scrollToTop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ScrollToTop from './components/ScrollToTop'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
@@ -9,6 +10,7 @@ import store from './store'
 ReactDOM.render(
   <Provider store={store}>
     <Router>
+      <ScrollToTop></ScrollToTop>
       <App />
     </Router>
   </Provider>,
