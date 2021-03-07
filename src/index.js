b1d8cfc69edb88544f4f531e5de4859a4924abde
unknown <akhilice13@gmail.com>
2021-03-08 01:12:53 +0530

    Added the particles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ScrollToTop from './components/ScrollToTop'
+import ParticleComponent from './components/ParticleComponent'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App'
@@ -11,7 +12,8 @@ ReactDOM.render(
   <Provider store={store}>
     <Router>
       <ScrollToTop></ScrollToTop>
-      <App />
+      <ParticleComponent/>
+      <App></App>
     </Router>
   </Provider>,
   document.getElementById('root')
